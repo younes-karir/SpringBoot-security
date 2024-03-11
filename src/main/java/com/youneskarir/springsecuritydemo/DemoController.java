a7aa9098d9670cc2f851852b7361b8212772c0f3
younes-karir <youneskarir@outlook.com>
2024-03-11 13:56:25 +0000

    testing spring security

---
+++ src/main/java/com/youneskarir/springsecuritydemo/DemoController.java
@@ -0,0 +1,33 @@
+package com.youneskarir.springsecuritydemo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    
+    
+    @GetMapping
+    public String getDemo(){
+        return "<h1>spring security test demo</h1>";
+    }
+
+    @GetMapping("one")
+    public String getDemoOne(){
+        return "<h1>spring security test demo ONE</h1>";
+    }
+
+    @GetMapping("two")
+    public String getDemoTwo(){
+        return "<h1>spring security test demo TWO</h1>";
+    }
+
+    @GetMapping("three")
+    public String getDemoThree(){
+        return "<h1>spring security test demo THREE</h1>";
+    }
+}
