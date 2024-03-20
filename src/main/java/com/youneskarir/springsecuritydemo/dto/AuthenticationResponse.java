1afef7c72ae2ef7de349ec6ee97badfc60ad1170
younes-karir <youneskarir@outlook.com>
2024-03-20 22:16:26 +0000

    creating authentication response entity

---
+++ src/main/java/com/youneskarir/springsecuritydemo/dto/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.youneskarir.springsecuritydemo.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationResponse {
+    
+    private String token;
+}
