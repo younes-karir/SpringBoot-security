52dbf7d5ac0a67ade72b8c7a98e1d4e4a3e7bf85
younes-karir <youneskarir@outlook.com>
2024-03-25 21:29:10 +0000

    adding validation

--- src/main/java/com/youneskarir/springsecuritydemo/dto/AuthenticationRequest.java
+++ src/main/java/com/youneskarir/springsecuritydemo/dto/AuthenticationRequest.java
@@ -1,6 +1,9 @@
 package com.youneskarir.springsecuritydemo.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +15,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationRequest {
 
+    
+    @NotBlank(message = "email is empty")
+    @Email(message = "not valid email format")
     private String email;
+    
+    @NotBlank(message = "password is empty")
+    @Size(min = 8,message = "password less than 8 characters")
     private String password;
 }
