77447ee4d14191d0e66e6d34d1d96ea41fa71c04
younes-karir <youneskarir@outlook.com>
2024-04-13 17:40:01 +0000

    updating the Authentication Service

--- src/main/java/com/youneskarir/springsecuritydemo/config/AuthenticationService.java
+++ src/main/java/com/youneskarir/springsecuritydemo/config/AuthenticationService.java
@@ -4,14 +4,14 @@ package com.youneskarir.springsecuritydemo.config;
 import com.youneskarir.springsecuritydemo.dto.AuthenticationRequest;
 import com.youneskarir.springsecuritydemo.dto.AuthenticationResponse;
 import com.youneskarir.springsecuritydemo.dto.RegisterRequest;
-import com.youneskarir.springsecuritydemo.model.Role;
 import com.youneskarir.springsecuritydemo.model.User;
 import com.youneskarir.springsecuritydemo.repository.UserRepository;
+import com.youneskarir.springsecuritydemo.token.Token;
+import com.youneskarir.springsecuritydemo.token.TokenRepository;
+import com.youneskarir.springsecuritydemo.token.TokenType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +23,7 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final TokenRepository tokenRepository;
     
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -32,8 +33,9 @@ public class AuthenticationService {
                 .role(request.getRole())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .build();
-        userRepository.save(user);
+        var savedUser = userRepository.save(user);
         var jwtToken = jwtService.generateToken(user);
+        persistToken(savedUser,jwtToken);
         return  AuthenticationResponse.builder()
                 .token(jwtToken)
                 .role(user.getRole())
@@ -49,9 +51,35 @@ public class AuthenticationService {
         );
         var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
         var jwtToken = jwtService.generateToken(user);
-        return  AuthenticationResponse.builder()
+        revokeUserTokens(user);
+        persistToken(user,jwtToken);
+        return  AuthenticationResponse.builder() 
                 .token(jwtToken)
                 .role(user.getRole())
                 .build();
+    }
+    
+    private void persistToken(User user,String jwtToken){
+        
+        var token = Token.builder()
+                .token(jwtToken)
+                .user(user)  /// the reference of the owner
+                .expired(false)
+                .revoked(false)
+                .tokenType(TokenType.BEARER)
+                .build();
+        tokenRepository.save(token);
+    }
+    
+    private void revokeUserTokens(User user){
+        var validUserTokens=tokenRepository.findAllValidTokensByUser(user.getId());
+        if(validUserTokens.isEmpty()){
+            return;
+        }
+        validUserTokens.forEach(item -> {
+            item.setRevoked(true);
+            item.setExpired(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
     }
 }
