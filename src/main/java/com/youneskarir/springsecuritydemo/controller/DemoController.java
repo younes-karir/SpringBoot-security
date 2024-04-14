cf2ecf1d0a8277e110bf174645df0aa790301e3a
younes-karir <youneskarir@outlook.com>
2024-04-14 16:11:38 +0100

    updating demo controller

--- src/main/java/com/youneskarir/springsecuritydemo/controller/DemoController.java
+++ src/main/java/com/youneskarir/springsecuritydemo/controller/DemoController.java
@@ -16,19 +16,22 @@ public class DemoController {
         return "<h1>spring security test demo</h1>";
     }
     
+    /*
     @GetMapping("one")
-    @PreAuthorize("hasRole('USER')")
+    //@PreAuthorize("hasRole('USER')")
     public String getDemoOne(){
         return "<h1>you are a user</h1>";
     }
     
     @GetMapping("two")
-    @PreAuthorize("hasRole({'ADMIN','USER'})")
-    public String getDemoTwo(){
+    //@PreAuthorize("hasRole({'ADMIN','USER'})")
+     public String getDemoTwo(){
         return "<h1>you are an admin</h1>";
     }
+    
     @GetMapping("three")
     public String getDemoThree(){
         return "<h1>spring security test demo THREE</h1>";
     }
+    */
 }
