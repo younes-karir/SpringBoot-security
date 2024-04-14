1edc522d25147ac476269fadf5f5e6f245a7bfd9
younes-karir <youneskarir@outlook.com>
2024-04-14 16:11:50 +0100

    updating exception handler

--- src/main/java/com/youneskarir/springsecuritydemo/advice/ExceptionHandle.java
+++ src/main/java/com/youneskarir/springsecuritydemo/advice/ExceptionHandle.java
@@ -1,8 +1,13 @@
 package com.youneskarir.springsecuritydemo.advice;
 
 
+import com.youneskarir.springsecuritydemo.advice.custom.NotValidTokenException;
+import com.youneskarir.springsecuritydemo.response.ResponseHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -22,4 +27,15 @@ public class ExceptionHandle {
         );
         return errors;
     }
+    
+    
+//    @ExceptionHandler(NotValidTokenException.class)
+//    @ResponseStatus(HttpStatus.FORBIDDEN)
+//    public ResponseEntity<Object> handleNotValidTokenException(NotValidTokenException exception){
+//        return ResponseHandler.build(
+//                exception.getMessage(),
+//                HttpStatus.FORBIDDEN,
+//                null
+//        );
+//    }
 }
