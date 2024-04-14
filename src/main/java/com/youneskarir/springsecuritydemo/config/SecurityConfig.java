ae845d0a1b4d7d1d04f3592e658b7f2c84a0a76b
younes-karir <youneskarir@outlook.com>
2024-04-14 16:12:08 +0100

    updating security configuration

--- src/main/java/com/youneskarir/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/youneskarir/springsecuritydemo/config/SecurityConfig.java
@@ -10,8 +10,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -21,25 +24,26 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeRequests((requests) -> requests
-                        .requestMatchers("/auth/authenticate", "/auth/register").permitAll()
-                        //.requestMatchers("demo/one").hasAuthority("USER")
-                       // .requestMatchers("demo/two").hasAuthority("ADMIN")
-                        .anyRequest().authenticated()
-                );
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/auth/authenticate", "/auth/register").permitAll();
+                    //.requestMatchers("demo/one").hasAuthority("USER")
+                    // .requestMatchers("demo/two").hasAuthority("ADMIN")
+                    authorize.anyRequest().authenticated();
+                });
+               
         http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authenticationProvider(authenticationProvider);
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        http.logout(log-> {
-                    log.clearAuthentication(true)
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
-                    .logoutSuccessUrl("/auth/login?logout")
-                    .permitAll();
-        });
+        http.logout((logout) -> {
+            logout.logoutUrl("/auth/logout");
+            logout.addLogoutHandler(logoutHandler); // TODO : create the logout handler to handle the logic 
+            logout.logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()));
+            });
         return http.build();
     }
 
