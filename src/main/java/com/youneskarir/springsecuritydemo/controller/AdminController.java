854bbfcc98b6ada0fb97b6f1c99634ecde46e9ae
younes-karir <youneskarir@outlook.com>
2024-04-21 15:08:45 +0100

    update the AdminController use the @PreAuthorized annotation.

--- src/main/java/com/youneskarir/springsecuritydemo/controller/AdminController.java
+++ src/main/java/com/youneskarir/springsecuritydemo/controller/AdminController.java
@@ -1,28 +1,34 @@
 package com.youneskarir.springsecuritydemo.controller;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/admin")
+@PreAuthorize("hasAnyRole('ADMIN')")
 public class AdminController {
 
     @GetMapping
+    @PreAuthorize("hasAuthority('admin:read')")
     public String get(){
         return "GET:: admin controller";
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('admin:create')")
     public String post(){
         return "POST:: admin controller";
     }
 
     @PutMapping
+    @PreAuthorize("hasAuthority('admin:update')")
     public String put(){
         return "PUT:: admin controller";
     }
 
     @DeleteMapping
+    @PreAuthorize("hasAuthority('admin:delete')")
     public String delete(){
         return "DELETE:: admin controller";
     }
